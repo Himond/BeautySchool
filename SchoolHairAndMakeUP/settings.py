1e20ddfc2dd8ea6202164bc1967379ded3966cbf
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2020-03-25 12:06:03 +0300

    Second version of application moved into github

--- SchoolHairAndMakeUP/settings.py
+++ SchoolHairAndMakeUP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '19r4=ri$1-y-r)vrb%-4))%%iv93f7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
